5aacc28e99316c52328d63ac8c8928da75be28a1
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2022-08-11 20:45:48 +0200

    Error handling for writing the Windows registry when not having admin permissions

--- libraries/windows_registry/windows_registry.ts
+++ libraries/windows_registry/windows_registry.ts
@@ -1,58 +1,50 @@
 export class WindowsRegistry {
-
-    async execute(command: string, args: string[]): Promise<string> {
-      const finalCommand = [
-        "powershell",
-        "-Command",
-        command,
-        ...args,
-      ];
-        finalCommand.forEach((arg, index) => {
-            if (typeof arg !== "string") {
-                arg = arg + ""
-                finalCommand[index] = arg
-            }
-            if (arg.includes(" ")) {
-                finalCommand[index] = `"${arg}"`;
-            }
-        })
-
-      const p = Deno.run({
-        cmd: finalCommand,
-        quoteArgs: true,
-        stderr: "piped",
-        stdout: "piped",
-      });
-      let output;
-      try {
-        const status = await p.status();
-        if (status.code !== 0) {
-          console.error(`${status.code}}`);
-        }
-        output = await p.output();
-        const err = await p.stderrOutput();
-        if (err) {
-            throw new TextDecoder().decode(err);
-        }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        p.close();
+  async execute(command: string, args: string[]): Promise<string> {
+    const finalCommand = [
+      "powershell",
+      "-Command",
+      command,
+      ...args,
+    ].map((arg, _index) => {
+      if (typeof arg !== "string") {
+        arg = arg + "";
       }
-  
-      return new TextDecoder().decode(output);
-    }
-  
-  
-    async write(path: string, name: string, value: string): Promise<void> {
-      await this.execute("Set-ItemProperty", [path, name, value])
+      if (arg.includes(" ")) {
+        arg = `"${arg}"`;
+      }
+      return arg;
+    });
+
+    const p = Deno.run({
+      cmd: finalCommand,
+      quoteArgs: true,
+      stderr: "piped",
+      stdout: "piped",
+    });
+
+    const output = await p.output();
+    const err = await p.stderrOutput();
+    p.close();
+    if (err.length) {
+      throw new TextDecoder().decode(err);
     }
 
-    async createFolder(path: string): Promise<void> {
-        if (await this.execute("Test-Path", [path]) == "False") {
-            await this.execute("New-Item", ["-Path", path, "-type", "Folder"]);
-        }
-    }
-  
+    return new TextDecoder().decode(output);
   }
-  
+
+  async write(path: string, name: string, value: string): Promise<void> {
+    try {
+      await this.execute("Set-ItemProperty", [path, name, value]);
+    } catch (err) {
+      if (String(err).match("registry access is not allowed")) {
+        console.warn(`Skipping, registry access is not allowed for ${path} with name ${name} and value ${value}`);
+      }
+    }
+  }
+
+  async createFolder(path: string): Promise<void> {
+    if (await this.execute("Test-Path", [path]) == "False") {
+      await this.execute("New-Item", ["-Path", path, "-type", "Folder"]);
+    }
+  }
+}
